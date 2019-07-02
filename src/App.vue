b46f91a234a5ef21d072ba60abe77893da859a8b
atlansien <atlansien@yahoo.co.jp>
2019-07-02 14:13:46 +0900

    add:sendMail

--- src/App.vue
+++ src/App.vue
@@ -31,10 +31,10 @@
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="sendmail">
-              <input type="checkbox" id="sendmail" value="SendMail" /> Send Mail
+              <input type="checkbox" id="sendmail" value="SendMail" v-model="sendMail"/> Send Mail
             </label>
             <label for="sendInfomail">
-              <input type="checkbox" id="sendInfomail" value="SendInfoMail" /> Send Infomail
+              <input type="checkbox" id="sendInfomail" value="SendInfoMail" v-model="sendMail"/> Send Infomail
             </label>
           </div>
         </div>
@@ -80,7 +80,7 @@
               <strong>Send Mail?</strong>
             </p>
             <ul>
-              <li></li>
+              <li v-for="item in sendMail">{{ item }}</li>
             </ul>
             <p>Gender:</p>
             <p>Priority:</p>
@@ -101,7 +101,8 @@ export default {
 				password: "",
 				age: 27
 			},
-			message: "A New text"
+			message: "A New text",
+			sendMail: [],
 		}
   }
 };
